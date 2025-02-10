7dfbeffc1c437582369aeccbb5899441010c97a5
alphacoder-mp3 <shindeakshay223@gmail.com>
2025-02-11 03:39:44 +0530

    Feat: add motion wrappers for animation

---
+++ motion-wrappers/index.ts
@@ -0,0 +1,4 @@
+export * from '@/motion-wrappers/motion-div';
+export * from '@/motion-wrappers/motion-nav';
+// export * from '@/motion-wrappers/motion-image';
+// export * from '@/motion-wrappers/motion-section';
---
+++ motion-wrappers/motion-nav.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { motion, MotionProps } from 'framer-motion';
+
+export const MotionNav = ({
+  children,
+  className,
+  initial,
+  animate,
+  onClick,
+  ...props
+}: {
+  children: React.ReactNode;
+  className: string;
+  initial: { opacity?: number; y?: number; x?: number };
+  animate: { opacity?: number; y?: number; x?: number };
+  variant?: {
+    hidden: { opacity?: number; y?: number; x?: number };
+    show: { opacity?: number; y?: number; x?: number };
+  };
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+} & MotionProps) => {
+  return (
+    <motion.nav
+      initial={initial}
+      animate={animate}
+      className={className}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </motion.nav>
+  );
+};
+
+/**
+ * A wrapper component for `motion.div` from Framer Motion.
+ * This component applies initial and animate properties for animations
+ * and accepts children to be rendered inside the motion div.
+ *
+ * @param children - The content to be rendered inside the motion div.
+ * @param className - The Tailwind CSS class names to be applied to the motion div.
+ * @param initial - The initial animation state with opacity and y-axis position.
+ * @param animate - The animate state with opacity and y-axis position.
+ * @param onClick - A click event handler for the motion div.
+ * @param props - Additional props for `motion.div` (e.g., transition, exit, etc.).
+ *
+ * @example
+ * ```tsx
+ * <MotionDiv
+ *   className="bg-blue-500 p-4"
+ *   initial={{ opacity: 0, y: -10 }}
+ *   animate={{ opacity: 1, y: 0 }}
+ *   onClick={() => console.log('Clicked!')}
+ * >
+ *   <p>Hello, world!</p>
+ * </MotionDiv>
+ * ```
+ */
