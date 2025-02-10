e35e2f62ce2c69935c658d3758c9144da17de57a
alphacoder-mp3 <shindeakshay223@gmail.com>
2025-02-11 03:40:24 +0530

    Add site config

---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+  name: 'FlowSync - A productivity tool for everyone',
+  siteName: 'FlowSync',
+  url: '',
+  twitterHandle: '@indiehash2',
+  twitterURL: 'https://twitter.com/indiehash2',
+  ogImage: '',
+  siteLogo: '',
+  description: '',
+  email: '',
+  links: {
+    x: '',
+    github: '',
+    linkedin: '',
+    youtube: '',
+    discord: '',
+  },
+  keywords: [],
+};
+
+export type SiteConfig = typeof siteConfig;
