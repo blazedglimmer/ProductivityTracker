df3cab548c252e668f3d469f141312207387b816
alphacoder-mp3 <shindeakshay223@gmail.com>
2025-05-13 12:27:10 +0530

    Remove jsx snippet from notes section

--- app/(app)/notes/page.tsx
+++ app/(app)/notes/page.tsx
@@ -24,11 +24,6 @@ export default async function Home() {
 
   return (
     <div className="container mx-auto p-4 m-12 max-h-screen w-full">
-      <div className={'flex items-center justify-center mb-6'}>
-        <div className="sm:hidden text-2xl md:text-4xl bg-gradient-to-r from-purple-600 via-indigo-600 to-pink-600 px-4 py-2 rounded-md w-fit italic text-white">
-          ByteNotes.
-        </div>
-      </div>
       <CreateNotes userId={userId} />
       <GetNotes userId={userId} />
     </div>
