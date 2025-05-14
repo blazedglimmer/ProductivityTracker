a0eb9e0c28b62a1b166d29657a47da12c0fb9559
alphacoder-mp3 <shindeakshay223@gmail.com>
2025-05-14 17:18:55 +0530

    Remove redundant collaborator fetching logic in GetNotes component

--- components/notes/get-notes.tsx
+++ components/notes/get-notes.tsx
@@ -10,7 +10,6 @@ import {
 import { ListingCard } from '@/components/notes/listing-card';
 import Image from 'next/image';
 import { getLightModeColor, getDarkModeColor } from '@/common/notes/common';
-import { getCollaborators } from '@/app/actions/collaborate-actions';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const GetNotes = async ({
@@ -31,72 +30,60 @@ const GetNotes = async ({
     <span> Error while fetching todos {message}</span>;
   }
 
-  // Get all collaborators for the todos
-  const todosWithCollaborators = await Promise.all(
-    todo.map(async item => {
-      const { data: collabs } = await getCollaborators(item.id);
-      return { ...item, collaborators: collabs };
-    })
-  );
-
   return (
     <section className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 p-4 mt-10">
-      {todosWithCollaborators?.map(item => {
-        // const { data: collabs, success } = await getCollaborators(item.id);
-
-        return (
-          <ListingCard
-            key={item.id}
-            item={item}
-            userId={userId}
-            className={`relative mb-4 border dark:border-slate-700 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 break-inside-avoid-column group ${getLightModeColor(
-              item.todoColor
-            )} ${getDarkModeColor(item.todoColor)}`}
-            collabs={item.collaborators}
-          >
-            <CardHeader className="py-2 px-4">
-              {item.images?.map((item: { id: string; url: string }) => (
-                <Image
-                  src={item.url}
-                  key={item.id}
-                  alt="Image"
-                  width={200}
-                  height={200}
-                  className="h-full w-full rounded"
-                  priority={true}
-                />
+      {todo?.map(item => (
+        <ListingCard
+          key={item.id}
+          item={item}
+          userId={userId}
+          className={`relative mb-4 border dark:border-slate-700 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 break-inside-avoid-column group ${getLightModeColor(
+            item.todoColor
+          )} ${getDarkModeColor(item.todoColor)}`}
+          collabs={item.collaborators}
+        >
+          <CardHeader className="py-2 px-4">
+            {item.images?.map((item: { id: string; url: string }) => (
+              <Image
+                src={item.url}
+                key={item.id}
+                alt="Image"
+                width={200}
+                height={200}
+                className="h-full w-full rounded"
+                priority={true}
+              />
+            ))}
+            <CardTitle className="font-bold text-lg break-words">
+              {item.title}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="py-2 px-4 break-words">
+            {parseFormattedText(item.description)}
+          </CardContent>
+          <CardFooter className="mb-4">
+            {item.collaborators
+              ?.filter(item => item.user.id !== userId)
+              .map(item => (
+                <Avatar
+                  key={item.user.id}
+                  className="flex items-center justify-center"
+                >
+                  <AvatarImage
+                    src={item.user.image as string}
+                    alt="AS"
+                    className="cursor-pointer w-7 h-7 rounded-full"
+                    width={50}
+                    height={50}
+                  />
+                  <AvatarFallback className="cursor-pointer w-7 h-7 p-2 shadow rounded-full dark:border border-gray-600 text-xs">
+                    {item.user?.name?.charAt(0)}
+                  </AvatarFallback>
+                </Avatar>
               ))}
-              <CardTitle className="font-bold text-lg break-words">
-                {item.title}
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="py-2 px-4 break-words">
-              {parseFormattedText(item.description)}
-            </CardContent>
-            <CardFooter className="mb-4">
-              {item.collaborators
-                ?.filter(item => item.user.id !== userId)
-                .map(item => (
-                  <Avatar
-                    key={item.user.id}
-                    className="flex items-center justify-center"
-                  >
-                    <AvatarImage
-                      src={item.user.image as string}
-                      alt="AS"
-                      className="cursor-pointer w-7 h-7 rounded-full"
-                      width={50}
-                      height={50}
-                    />
-                    <AvatarFallback className="cursor-pointer w-7 h-7 p-2 shadow rounded-full dark:border border-gray-600 text-xs">
-                      {item.user?.name?.charAt(0)}
-                    </AvatarFallback>
-                  </Avatar>
-                ))}
-            </CardFooter>
-          </ListingCard>
-        );
-      })}
+          </CardFooter>
+        </ListingCard>
+      ))}
     </section>
   );
 };
